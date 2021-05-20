df532ec3e9c4a0c494a29ac160444e02fd6a53fd
franklin.farias <franklin.farias@hospitaldocoracao-al.com.br>
2021-05-19 22:26:48 -0300

    adjustement mat-selected initalized with value; created snack-alert;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { MatOptionModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import {MatSnackBarModule} from "@angular/material/snack-bar";
 
 
 @NgModule({
@@ -39,7 +40,8 @@ import { MatMenuModule } from '@angular/material/menu';
     MatSelectModule,
     MatOptionModule,
     MatIconModule,
-    MatMenuModule
+    MatMenuModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/movie/movie-form/movie-form.component.ts
+++ src/app/movie/movie-form/movie-form.component.ts
@@ -1,5 +1,7 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {Component, Inject, Input, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material/dialog';
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {MovieListComponent} from "../movie-list/movie-list.component";
 
 export interface DialogData {
     numeroFilme: number;
@@ -24,10 +26,28 @@ export interface DialogData {
 })
 export class MovieFormComponent implements OnInit {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) {
+  selectedTipoProduto = '';
+  selectedTipoFilme = '';
+  selectedGenero = '';
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData, private snackBar: MatSnackBar, public dialogRef: MatDialogRef<MovieListComponent>) {
   }
 
   ngOnInit(): void {
+    this.selectedTipoProduto = this.data.tipoProduto;
+    this.selectedTipoFilme = this.data.tipoFilme;
+    this.selectedGenero = this.data.genero;
+
+    console.log(this.selectedGenero);
+    console.log(this.selectedTipoProduto);
+    console.log(this.selectedTipoFilme);
   }
 
+  openSnackBar(): void {
+    this.dialogRef.close();
+    this.snackBar.open('Título salvo com sucesso!', '', {
+      duration: 2000,
+      panelClass: 'success-alert-message-component'
+    });
+  }
 }
